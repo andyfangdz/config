a8f75a2b385bcbc2c634ff7d5e31f8d129cd371f
Andy Fang <andyfang.dz@gmail.com>
2016-07-15 10:45:55 -0400

    Add functions

--- functions.sh
+++ functions.sh
@@ -1,2 +1,33 @@
 activate_conda() { export PATH=~/anaconda/bin:$PATH }
 activate_conda2() { export PATH=~/anaconda2/bin:$PATH }
+
+
+# http://apple.stackexchange.com/questions/12161/os-x-terminal-must-have-utilities
+
+update() {
+    local brew="brew update; brew upgrade;"
+    local gem="gem update;"
+    local pip="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U -q"
+    sh -c $brew$pip; sudo sh -c $gem
+}
+
+cdf() {
+    target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
+    if [ "$target" != "" ]; then
+        cd "$target"; pwd
+    else
+        echo 'No Finder window found' >&2
+    fi
+}
+
+man() {
+    env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
+}
