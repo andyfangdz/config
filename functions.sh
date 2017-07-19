a64d8361048c31cfb3c907b3f70580429f648995
Dezhi (Andy) Fang <dezhi.fang@airbnb.com>
2017-07-18 23:52:52 -0700

    Temp. remove anaconda

--- functions.sh
+++ functions.sh
@@ -38,3 +38,15 @@ function anybar { echo -n $1 | nc -4u -w0 localhost ${2:-1738}; }
 unformat() {
     pbpaste | pbcopy
 }
+
+
+function upload_gem {
+    username=`whoami`
+    # resolve the DNS entry only once,
+    # otherwise the commands might go to different hosts
+    host=$(dig +short geminabox-internal.aws.airbnb.com|head -n1|sed s'/\.$//')
+  
+    # Upload to gem server and add it
+    scp $1 ${username}@${host}:~ && \
+        ssh ${username}@${host} "add_gem.rb $1; rm $1"
+}
--- python.sh
+++ python.sh
@@ -1,2 +1,2 @@
 alias ipy='jupyter qtconsole --ConsoleWidget.font_family="Consolas" --ConsoleWidget.font_size=13'
-export PATH=~/miniconda2/bin:$PATH
+# export PATH=~/miniconda2/bin:$PATH
